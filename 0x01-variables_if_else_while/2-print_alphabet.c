debbc5a1131cbc1a7dcf65dbc7dbb1668198e195
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-10 01:32:10 +0200

    my first com

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'prints alphabet in lowercase'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n[] = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar(n);
+
+	return (0);
+}
