2e983de45d9baa667d475f2ae477a76b8c4a1ba4
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-10 03:32:39 +0200

    my first com

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,19 +11,17 @@
  */
 int main(void)
 {
-        int a;
+	int a;
 
-        for (a = '0'; a <= '9'; a++)
-        {
-                putchar(a);
-        {
-                putchar(',');
-                putchar(' ');
-        }
-        }
-
-        putchar('\n');
-
-        return (0);
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		if (a != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-      
