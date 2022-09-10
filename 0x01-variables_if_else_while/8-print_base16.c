c0b1ebb418f3e8d929b1c4b4ef07ef6d4820f099
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-10 02:59:07 +0200

    my first com

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'prints all single digit numbets of base 10 starting from 0'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '15'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
