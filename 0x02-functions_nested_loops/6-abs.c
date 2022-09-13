ba7cace81b135f5f38836f8b10ea0c081545a048
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-13 18:27:44 -0400

    task 6 commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _abs(int)- computes absolute value of an integer
- * 'int' -> input
+ * _abs(int a)- computes absolute value of an integer
+ * 'int a' -> input
  * Return: int.
  */
 int _abs(int a)
