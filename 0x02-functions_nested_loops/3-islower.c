2b235a21b9f5410f56ea75bfccd63d08186d642e
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-13 17:34:28 -0400

    task 3 commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * _islower(int c) -> returns lower case
+ * return: returns 1 or 0
+ */
+int _islower(int c)
+{
+	if (islower('c'))
+	{
+		putchar('1');
+	}
+	else
+	{
+		putchar('0');
+	}
+}
