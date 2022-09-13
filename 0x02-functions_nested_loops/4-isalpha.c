d5c972bc5522842f3e20332b95473307ce76dac7
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-13 19:15:00 -0400

    task 2 commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
-  * int _isalpha(int c); -> checks alphabetic character.
-  */
+ * _isalpha - check the code.
+ * @c: argument passed
+ * Return: Always 0.
+ */
 int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
