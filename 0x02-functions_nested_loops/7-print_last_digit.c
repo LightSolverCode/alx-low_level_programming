66f173ee6e01ae7a4e15a648671f198dc5ea76d7
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-13 18:42:54 -0400

    task 7 commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - check the code
+ * @a: the argument
+ * Return: Always 0.
+ */
+int print_last_digit(int a)
+{
+	int x = a % 10;
+
+	putchar(x);
+
+	return (0);
+}
