c7f84610d7f3b2e6dec9a520f1fba52e18570aca
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-13 14:04:01 -0400

    task 1 commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Description: 'the program's description'
+ * print_alphabet - prints lowercase letter
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet(void)/*print_alphabet in lowercase*/
 {
 	char ch;
 
