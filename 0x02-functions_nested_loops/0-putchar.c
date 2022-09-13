a9cf391ace21857e34c01620202985f62d0ba52a
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-13 17:35:25 +0200

    my first com

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * Description: print a word with putchar
+ * Description: print a letter with putchar
+ *
  * return: 0;
  */
-int main(void)/*main function*/
+int main(void)
+/*main function*/
 {
-		putchar('_');
-		putchar('p');
-		putchar('u');
-		putchar('t');
-		putchar('c');
-		putchar('h');
-		putchar('a');
-		putchar('r');
-		putchar('\n');
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
 
 		return (0);
 }
