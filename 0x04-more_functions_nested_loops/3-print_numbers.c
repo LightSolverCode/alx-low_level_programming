69e756fd05da2040d454276b27458f69d2bc6b54
Peter Buckingham <peterbuckingham177@gmail.com>
2022-09-16 09:59:23 -0400

    My task 3 commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a >= 10; a++)
+	{
+		putchar(a);
+	}
+}
